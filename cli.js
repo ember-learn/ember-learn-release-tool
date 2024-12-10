5d3ea1d30059a485afe673c183954efc4aca2404
Chris Manson <chris@manson.ie>
2024-12-10 23:41:41 +0000

    add guides-search command

--- cli.js
+++ cli.js
@@ -10,6 +10,7 @@ const pkg = JSON.parse(
 );
 
 import guides from './projects/guides.js';
+import guidesSearch from './projects/guides-search.js';
 
 program
   .name(pkg.name)
@@ -25,6 +26,16 @@ program
   .description('Deploy the new version for https://guides.emberjs.com')
   .action((args, commandOptions) =>
     guides(args, {
+      ...program.opts(),
+      ...commandOptions,
+    }),
+  );
+
+program
+  .command('guides-search')
+  .description('Update Algolia indexes for https://guides.emberjs.com')
+  .action((args, commandOptions) =>
+    guidesSearch(args, {
       ...program.opts(),
       ...commandOptions,
     }),
